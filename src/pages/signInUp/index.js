674e7b730276926f8a72a474985d35d7163cef3c
hung211097 <1512227@student.hcmus.edu.vn>
2018-11-30 19:07:40 +0700

    complete sign In & Up page

--- src/pages/signInUp/index.js
+++ src/pages/signInUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './index.scss';
+import {Footer} from '../../components'
 
 class SignInUp extends Component {
   render() {
@@ -8,18 +9,7 @@ class SignInUp extends Component {
         <div className="parallax filter-black">
           <div className="parallax-image" />
           <div className="small-info">
-            <div className="row col-md-22 hidden">
-              <form action method="POST" autoComplete="off">
-                <input type="text" id="short" name placeholder="Username or email" />
-                <input type="password" id="short" name placeholder="Password" />
-                {/*<input type="checkbox" name="remember" value="1">*/}
-                <a type="submit" href="profile.html" name="login" className="btn btn-danger">Login</a>
-                <span className="forgot-password-link">
-                  <a href="#">Forgot your password?</a><br />
-                </span>
-              </form>
-            </div>
-            <div className="col-sm-10 col-sm-push-1 col-md-6 col-md-push-3 col-lg-6 col-lg-push-3">
+            <div className="col-sm-10 col-sm-push-1 col-md-6 col-md-push-3 col-lg-6 col-lg-push-3 mx-auto">
               <div className="card-group animated flipInX">
                 <div className="card">
                   <div className="card-block">
@@ -33,56 +23,18 @@ class SignInUp extends Component {
                       </div>
                       <div className="form-group">
                         <input type="password" className="form-control" placeholder="Password" />
-                        <a href="#" className="pull-xs-right">
+                        {/*<a href="#" className="pull-xs-right">
                           <small>Forgot?</small>
-                        </a>
+                        </a>*/}
                         <div className="clearfix" />
                       </div>
                       <div className="center">
-                        <a href="profile.html" className="btn  btn-azure">
-                          Login
-                        </a>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-                <div className="card col-md-22 hidden">
-                  <div className="card-block">
-                    <div className="center">
-                      <h4 className="m-b-0"><span className="icon-text">Login</span></h4>
-                      <p className="text-muted">Access your account</p>
-                    </div>
-                    <form action="index.html" method="get">
-                      <div className="form-group">
-                        <input type="email" className="form-control" placeholder="Email Address" />
-                      </div>
-                      <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password" />
-                        <a href="#" className="pull-xs-right">
-                          <small>Forgot?</small>
-                        </a>
-                        <div className="clearfix" />
-                      </div>
-                      <div className="center">
-                        <a href="profile.html" className="btn  btn-azure">
-                          Login
-                        </a>
+                        <button type="submit" className="btn btn-azure">Login</button>
                       </div>
                     </form>
                   </div>
                 </div>
                 <div className="card">
-                  <div className="row col-md-22 hidden">
-                    <form action method="POST" autoComplete="off">
-                      <input type="text" id="short" name placeholder="Username or email" />
-                      <input type="password" id="short" name placeholder="Password" />
-                      {/*<input type="checkbox" name="remember" value="1">*/}
-                      <a type="submit" href="profile.html" name="login" className="btn btn-danger">Login</a>
-                      <span className="forgot-password-link">
-                        <a href="#">Forgot your password?</a><br />
-                      </span>
-                    </form>
-                  </div>
                   <div className="card-block center">
                     <h4 className="m-b-0">
                       <span className="icon-text">Sign Up</span>
@@ -109,6 +61,7 @@ class SignInUp extends Component {
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
