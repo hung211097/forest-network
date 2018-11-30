0c70fc71db3d47bd9e77db728963ba4b633c1606
hung211097 <1512227@student.hcmus.edu.vn>
2018-11-30 23:00:53 +0700

    add postbox

---
+++ src/components/post-box/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import styles from'./index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class PostBox extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      content: ''
+    }
+  }
+  render() {
+    return (
+      <div className={styles.postBox}>
+        <div className="box profile-info n-border-top">
+          <form>
+            <textarea className="form-control input-lg p-text-area" rows={2} placeholder="Whats in your mind today?" defaultValue={this.state.content} />
+          </form>
+          <div className="box-footer box-form">
+            <button type="button" className="btn btn-azure float-right">Post</button>
+            <ul className="nav nav-pills">
+              <li><a href="#"><i><FontAwesomeIcon icon="image"/></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostBox;
