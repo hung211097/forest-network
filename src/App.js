856181280bfcdcc8f82b828a3d0f4e2979deb57d
NTHungFitHcmus <nthungfithcmus@gmail.com>
2018-12-01 21:28:33 +0700

    add wallpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
-import {Home, SignInUp, ErrorPage} from './pages'
+import {Home, SignInUp, ErrorPage, Profile} from './pages'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faSignOutAlt, faUserFriends, faUsers, faNewspaper, faCalendarAlt, faImage, faThumbsUp, faShare, faUserPlus, faCheck } from '@fortawesome/free-solid-svg-icons'
-library.add(faHome, faSignOutAlt, faUserFriends, faUsers, faNewspaper, faCalendarAlt, faImage, faThumbsUp, faShare, faUserPlus, faCheck)
+import { faHome, faSignOutAlt, faUserFriends, faUsers, faNewspaper, faCalendarAlt, faImage, faThumbsUp, faShare, faUserPlus, faCheck, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab, faHome, faSignOutAlt, faUserFriends, faUsers, faNewspaper, faCalendarAlt, faImage, faThumbsUp, faShare, faUserPlus, faCheck, faEnvelope)
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
             <Redirect from='/home' to='/' />
             <Route exact path='/' component={Home} />
             <Route exact path='/login' component={SignInUp} />
+						<Route exact path='/profile' component={Profile} />
             <Route component={ErrorPage} />
           </Switch>
         </div>
