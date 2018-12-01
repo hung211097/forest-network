4dd5debaec3701dda7e7c5aedd6d045935270289
NTHungFitHcmus <nthungfithcmus@gmail.com>
2018-12-01 20:52:23 +0700

    Add profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
-import {Home, SignInUp, ErrorPage} from './pages'
+import {Home, SignInUp, ErrorPage, Profile} from './pages'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-library.add(faHome)
+import { faHome, faUserPlus, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons' 
+
+library.add(faHome, faUserPlus, faEnvelope, fab)
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
             <Redirect from='/home' to='/' />
             <Route exact path='/' component={Home} />
             <Route exact path='/login' component={SignInUp} />
+						<Route exact path='/profile' component={Profile} />
             <Route component={ErrorPage} />
           </Switch>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from './home'
 import SignInUp from './signInUp'
 import ErrorPage from './error'
+import Profile from './profile'
 
 export {
   Home,
   SignInUp,
-  ErrorPage
+  ErrorPage,
+	Profile
 }
