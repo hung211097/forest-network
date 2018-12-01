679480dc131032a3729fc2f8ad01eafe3505242e
hung211097 <1512227@student.hcmus.edu.vn>
2018-12-01 22:13:04 +0700

    add image offline

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,74 +2,79 @@ import React, { Component } from 'react';
 import styles from './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
+import { Layout } from '../../components'
+import avatar from '../../images/guy-3.jpg'
+import cover from '../../images/game.jpg'
 
 class Profile extends Component {
   render() {
     return (
-      <div className="container page-content">
-        <div className="row">
-          <div className="col-md-10 col-md-offset-1">
-            <div className="user-profile">
-              <div className="profile-header-background">
-                <img src="http://demos.bootdey.com/dayday/img/Cover/game.jpg" alt="Profile Header Background" />
-              </div>
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="profile-info-left">
-                    <div className="text-center">
-                      <img src="http://demos.bootdey.com/dayday/img/Friends/guy-3.jpg" alt="Avatar" className="avatar img-circle" />
-                      <h2>John Breakgrow jr.</h2>
-                    </div>
-                    <div className="action-buttons">
-                      <div className="row">
-                        <div className="col-xs-6">
-                          <a href="#" className="btn btn-azure btn-block"><FontAwesomeIcon icon="user-plus" /> Follow</a>
-                        </div>
-                        <div className="col-xs-6">
-                          <a href="#" className="btn btn-azure btn-block"><FontAwesomeIcon icon="envelope" /> Message</a>
+      <Layout>
+        <div className="container page-content">
+          <div className="row">
+            <div className="col-md-10 col-md-offset-1">
+              <div className="user-profile">
+                <div className="profile-header-background">
+                  <img src={cover} alt="Profile Header Background" />
+                </div>
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="profile-info-left">
+                      <div className="text-center">
+                        <img src={avatar} alt="Avatar" className="avatar img-circle" />
+                        <h2>John Breakgrow jr.</h2>
+                      </div>
+                      <div className="action-buttons">
+                        <div className="row">
+                          <div className="col-xs-6">
+                            <a href="#" className="btn btn-azure btn-block"><FontAwesomeIcon icon="user-plus" /> Follow</a>
+                          </div>
+                          <div className="col-xs-6">
+                            <a href="#" className="btn btn-azure btn-block"><FontAwesomeIcon icon="envelope" /> Message</a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="section">
-                      <h3>About Me</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ullamcorper ligula. Curabitur in sapien sed risus finibus condimentum et ac quam. Quisque eleifend, lacus ut commodo pulvinar, elit augue eleifend leo, eget suscipit augue erat id orci. .</p>
-                    </div>
-                    <div className="section">
-                      <h3>Statistics</h3>
-                      <p><span className="badge">332</span> Following</p>
-                      <p><span className="badge">124</span> Followers</p>
-                      <p><span className="badge">620</span> Likes</p>
-                    </div>
-                    <div className="section">
-                      <h3>Social</h3>
-                      <ul className="list-unstyled list-social">
-                        <li><a href="#"><FontAwesomeIcon icon={['fab', 'twitter']} /> @jhongrwo</a></li>
-                        <li><a href="#"><FontAwesomeIcon icon={['fab', 'facebook']} /> John grow</a></li>
-                        <li><a href="#"><FontAwesomeIcon icon={['fab', 'dribbble']} /> johninizzie</a></li>
-                        <li><a href="#"><FontAwesomeIcon icon={['fab', 'linkedin']} /> John grow</a></li>
-                      </ul>
-                      <ul className="list-group">
-                        <li className="list-group-item">
-                          <div id="carbonads"><span><span className="carbon-wrap"><a href="//srv.carbonads.net/ads/click/x/GTND42QWCKSDPKJECWYLYKQMCE7I5K3LCV7DPZ3JCWBDC2QWCYSD5K7KC6BIVK7NCWYI6K3EHJNCLSIZ?segment=placement:bootdeycom;" className="carbon-img" target="_blank" rel="noopener"><img src="https://cdn4.buysellads.net/uu/1/3386/1525189909-61450.png" alt border={0} height={100} width={130} style={{maxWidth: 130}} /></a><a href="//srv.carbonads.net/ads/click/x/GTND42QWCKSDPKJECWYLYKQMCE7I5K3LCV7DPZ3JCWBDC2QWCYSD5K7KC6BIVK7NCWYI6K3EHJNCLSIZ?segment=placement:bootdeycom;" className="carbon-text" target="_blank" rel="noopener">Adobe Creative Cloud for Teams starting at $29.99 per month.</a></span><a href="http://carbonads.net/?utm_source=bootdeycom&utm_medium=ad_via_link&utm_campaign=in_unit&utm_term=carbon" className="carbon-poweredby" target="_blank" rel="noopener">ads via Carbon</a></span></div>
-                        </li>
-                      </ul>
+                      <div className="section">
+                        <h3>About Me</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ullamcorper ligula. Curabitur in sapien sed risus finibus condimentum et ac quam. Quisque eleifend, lacus ut commodo pulvinar, elit augue eleifend leo, eget suscipit augue erat id orci. .</p>
+                      </div>
+                      <div className="section">
+                        <h3>Statistics</h3>
+                        <p><span className="badge">332</span> Following</p>
+                        <p><span className="badge">124</span> Followers</p>
+                        <p><span className="badge">620</span> Likes</p>
+                      </div>
+                      <div className="section">
+                        <h3>Social</h3>
+                        <ul className="list-unstyled list-social">
+                          <li><a href="#"><FontAwesomeIcon icon={['fab', 'twitter']} /> @jhongrwo</a></li>
+                          <li><a href="#"><FontAwesomeIcon icon={['fab', 'facebook']} /> John grow</a></li>
+                          <li><a href="#"><FontAwesomeIcon icon={['fab', 'dribbble']} /> johninizzie</a></li>
+                          <li><a href="#"><FontAwesomeIcon icon={['fab', 'linkedin']} /> John grow</a></li>
+                        </ul>
+                        <ul className="list-group">
+                          <li className="list-group-item">
+                            <div id="carbonads"><span><span className="carbon-wrap"><a href="//srv.carbonads.net/ads/click/x/GTND42QWCKSDPKJECWYLYKQMCE7I5K3LCV7DPZ3JCWBDC2QWCYSD5K7KC6BIVK7NCWYI6K3EHJNCLSIZ?segment=placement:bootdeycom;" className="carbon-img" target="_blank" rel="noopener"><img src="https://cdn4.buysellads.net/uu/1/3386/1525189909-61450.png" alt border={0} height={100} width={130} style={{maxWidth: 130}} /></a><a href="//srv.carbonads.net/ads/click/x/GTND42QWCKSDPKJECWYLYKQMCE7I5K3LCV7DPZ3JCWBDC2QWCYSD5K7KC6BIVK7NCWYI6K3EHJNCLSIZ?segment=placement:bootdeycom;" className="carbon-text" target="_blank" rel="noopener">Adobe Creative Cloud for Teams starting at $29.99 per month.</a></span><a href="http://carbonads.net/?utm_source=bootdeycom&utm_medium=ad_via_link&utm_campaign=in_unit&utm_term=carbon" className="carbon-poweredby" target="_blank" rel="noopener">ads via Carbon</a></span></div>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="col-md-8">
-                  <div className="profile-info-right">
-                    <ul className="nav nav-pills nav-pills-custom-minimal custom-minimal-bottom">
-                      <li className="active"><a href="#timeline" data-toggle="tab">Timeline</a></li>
-                      <li><a href="#followers" data-toggle="tab">Followers</a></li>
-                      <li><a href="#following" data-toggle="tab">Following</a></li>
-                    </ul>
+                  <div className="col-md-8">
+                    <div className="profile-info-right">
+                      <ul className="nav nav-pills nav-pills-custom-minimal custom-minimal-bottom">
+                        <li className="active"><a href="#timeline" data-toggle="tab">Timeline</a></li>
+                        <li><a href="#followers" data-toggle="tab">Followers</a></li>
+                        <li><a href="#following" data-toggle="tab">Following</a></li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Layout>
     );
   }
 };
